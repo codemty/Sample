3f78f117060f5123114682acb8a582852493c17b
firminor <1622305313@qq.com>
2020-09-29 09:57:58 +0800

    v0.1.12

--- vendor/sample/Loader.php
+++ vendor/sample/Loader.php
@@ -9,6 +9,28 @@ namespace sample;
 class Loader
 {
 	
+
+
+
+
+
+
+    /**
+     * 应用跟目录
+     * @Author   Tangy                    <1622305313@qq.com>
+     * @DateTime 2020-08-20T11:13:54+0800
+     * @version  [version]
+     * @return   [type]                   [description]
+     */
+    public static function getRootPath()
+    {
+
+
+    }
+
+
+
+
 	
     /**
      * 注册自动加载机制
@@ -18,7 +40,7 @@ class Loader
      */
     public static function register($autolaod = '')
     {
-        $autoload = $autoload ? $autolaod : 'sample\\Loader::autoLoad';
+        $autoload = $autoload ? : 'sample\\Loader::autoLoad';
         
         //注册系统自动加载函数
         spl_autoload_register($autoload, true, true);
@@ -50,10 +72,21 @@ class Loader
 	public static function autoLoad($class)
 	{
          
+        if (isset(self::$classAlias[$class])) {
+            return class_alias(self::$classAlias[$class], $class);
+        }
+
+        if ($file = self::findFile($class)) {
+
+            //严格区分大小写
+            if (strpos(PHP_OS, 'WIN') !== false && pathinfo($file, PATHINFO_FILENAME) !=) {
+                # code...
+            }
 
 
-
-
+            __include_file($file);
+            return true;
+        }
 	}
       
 
